97d217c47fe7e444a9d795b60f514ec922bcea5f
AhmadSyarwani <32427962+AhmadSyarwani@users.noreply.github.com>
2018-04-07 15:14:16 +0800

    cetak info

--- src/praktek3/computer.java
+++ src/praktek3/computer.java
@@ -2,4 +2,11 @@ package praktek3;
 public class computer {
         String merek,os,warna;
         double harga;
+        
+        void cetakInfo(){
+            System.out.println("merek komputer /t="+merek);
+            System.out.println("os komputer /t="+os);
+            System.out.println("warna komputer /t="+warna);
+            System.out.println("harga komputer /t="+harga);
+        }
 }
