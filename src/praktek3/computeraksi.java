d22de486bfc85a6ff5262a48d22b94c8887369a2
AhmadSyarwani <32427962+AhmadSyarwani@users.noreply.github.com>
2018-04-07 15:28:20 +0800

    method

--- src/praktek3/computeraksi.java
+++ src/praktek3/computeraksi.java
@@ -1,5 +1,12 @@
 package praktek3;
-
 public class computeraksi {
-    
+    public static void main(String[] args) {
+        computer laptop = new computer();
+        laptop.merek="asus";
+        laptop.os="win7";
+        laptop.warna="merah";
+        laptop.harga=6000;
+        
+        laptop.cetakInfo();
+    }    
 }
