858f50d4f7d915c2a77ce129d10db5eb39b9fbc5
AhmadSyarwani <32427962+AhmadSyarwani@users.noreply.github.com>
2018-04-07 16:16:32 +0800

    finish :alien:

--- src/praktek3/computeraksi.java
+++ src/praktek3/computeraksi.java
@@ -1,22 +1,33 @@
 package praktek3;
 public class computeraksi {
     public static void main(String[] args) {
-        computer laptop = new computer();
+        computer adul = new computer();
        
-        laptop.setMerek("asus");
-        laptop.setOs("win7");
-        laptop.setWarna("biru");
-        laptop.setHarga(6000);
+        adul.setMerek("asus");
+        adul.setOs("win7");
+        adul.setWarna("biru");
+        adul.setHarga(6000);
+        // ga jalan adul.setjenisComputer();
         
-        laptop.cetakInfo();
+        adul.cetakInfo();
         
         System.out.print("Mereknya \t= ");
-        System.out.println(laptop.getMerek());
-        System.out.print("Mereknya \t= ");
-        System.out.println(laptop.getOs());
-        System.out.print("Mereknya \t= ");
-        System.out.println(laptop.getWarna());
-        System.out.print("Mereknya \t= ");
-        System.out.println(laptop.getHarga());
+        System.out.println(adul.getMerek());
+        System.out.print("OS \t= ");
+        System.out.println(adul.getOs());
+        System.out.print("Warna \t= ");
+        System.out.println(adul.getWarna());
+        System.out.print("Harga \t= ");
+        System.out.println(adul.getHarga());
+        
+        laptop udin = new laptop();
+        udin.setMerek("acer");
+        udin.setOs("win10");
+        udin.setWarna("hitam");
+        udin.setHarga(100000);
+        udin.setJenisLaptop("acer212");
+        udin.cetakInfo();
+        System.out.println("Jenis \t="+udin.getJenisLaptop());
+        udin.throttle();
     }    
 }
