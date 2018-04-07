23e1c6070da87258dfa8b9937002e3d0fdde22d1
AhmadSyarwani <32427962+AhmadSyarwani@users.noreply.github.com>
2018-04-07 15:36:57 +0800

    getter and setter

--- src/praktek3/computer.java
+++ src/praktek3/computer.java
@@ -9,4 +9,37 @@ public class computer {
             System.out.println("warna komputer /t="+warna);
             System.out.println("harga komputer /t="+harga);
         }
+
+    public String getMerek() {
+        return merek;
+    }
+
+    public void setMerek(String merek) {
+        this.merek = merek;
+    }
+
+    public String getOs() {
+        return os;
+    }
+
+    public void setOs(String os) {
+        this.os = os;
+    }
+
+    public String getWarna() {
+        return warna;
+    }
+
+    public void setWarna(String warna) {
+        this.warna = warna;
+    }
+
+    public double getHarga() {
+        return harga;
+    }
+
+    public void setHarga(double harga) {
+        this.harga = harga;
+    }
+                
 }
